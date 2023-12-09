710d8d440f78bdac73e9efa615a9b8fa750ecf1e
rakhmatovich <nurmuxammadrahmatov@gmail.com>
2023-12-09 14:47:29 +0500

    added backend

--- frontend/src/components/Chelss.jsx
+++ frontend/src/components/Chelss.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 
+const news = [
+  {id: 1, text: 'Беспроцентная рассрочка действует до 30 ноября', url:'/sportage.jpg'},
+  {id: 2, text: 'Беспроцентная рассрочка на автомобили Kia Seltos и Kia K5', url:'/k5.jpg'},
+  {id: 3, text: 'Выгодные условия кредитования от Kia Uzbekistan', url:'/seltos.jpg'},
+  {id: 4, text: 'Выгодные условия кредитования от Kia Uzbekistan', url:'/stinger.jpg'},
+]
+
 export const Chelss = () => {
   return (
     <div className="chelss">
       <div className="chelss-h">
         <h3>Специальные предложения</h3>
       </div>
-      <div>
-        <img className="imiglar" src="/sportage.jpg" alt="" />
-        <img className="imiglar" src="/k5.jpg" alt="" />
-        <img className="imiglar" src="/seltos.jpg" alt="" />
-        <img className="imiglar" src="/stinger.jpg" alt="" />
-      </div>
       <div className="main-p">
-        <p className="chelss-p">Беспроцентная рассрочка действует до 30 ноября</p>
-        <p className="chelss-p">Беспроцентная рассрочка на автомобили Kia Seltos и Kia K5</p>
-        <p className="chelss-p">Выгодные условия кредитования от Kia Uzbekistan</p>
-        <p className="chelss-p">Выгодные условия кредитования от Kia Uzbekistan</p>
+
+        {news.map(item => (
+          <div className="chelss-item" key={item.id}>
+            <img src={item.url} alt={item.text} />
+            <p>{item.text}</p>
+          </div>
+        ))}
       </div>
-      <div >
+      <div className="chells-end">
          <button className="chells-b">Все спецпредложения</button>
       </div>
     </div>
